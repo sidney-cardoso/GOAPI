975d67e92c2f2a3ebcd2d875c4a32ef0295092c2
sidney-cardoso <sidneyrsc342@gmailcom>
2024-01-29 22:35:44 -0300

    READ e CREATE de Categories

---
+++ internal/database/category_db.go
@@ -0,0 +1,54 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com/sidney-cardoso/goapi/internal/entity"
+)
+
+type CategoryDB struct {
+	db *sql.DB
+}
+
+func NewCategoryDB(db *sql.DB) *CategoryDB {
+	return &CategoryDB{
+		db: db,
+	}
+}
+
+func (catDB *CategoryDB) GetCategories() ([]*entity.Category, error) {
+	rows, err := catDB.db.Query("SELECT id, name FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var categories []*entity.Category
+
+	for rows.Next() {
+		var category entity.Category
+		if err := rows.Scan(&category.ID, &category.Name); err != nil {
+			return nil, err
+		}
+		categories = append(categories, &category)
+	}
+	return categories, nil
+}
+
+func (catDB *CategoryDB) GetCategory(id string) (*entity.Category, error) {
+	var category entity.Category
+	err := catDB.db.QueryRow("SELECT id, name FROM categories WHERE id = ?", id).Scan(&category.ID, &category.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &category, nil
+}
+
+func (catDB *CategoryDB) CreateCategory(category *entity.Category) (string, error) {
+	_, err := catDB.db.Exec("INSERT INTO category VALUES (?, ?)", category.Name, category.Name)
+	if err != nil {
+		return "", err
+	}
+
+	return category.ID, nil
+}
