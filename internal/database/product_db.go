bed26d8a478f21949570cca8c934d751bacacf00
sidney-cardoso <sidneyrsc342@gmailcom>
2024-01-29 22:48:08 -0300

    Adição de campos no INSERT e SELECT

--- internal/database/product_db.go
+++ internal/database/product_db.go
@@ -15,7 +15,7 @@ func NewProductDB(db *sql.DB) *ProductDB {
 }
 
 func (prodDB *ProductDB) GetProducts() ([]*entity.Product, error) {
-	rows, err := prodDB.db.Query("SELECT id, name FROM products")
+	rows, err := prodDB.db.Query("SELECT id, name, description price, category_id, image_url FROM products")
 	if err != nil {
 		return nil, err
 	}
@@ -24,7 +24,7 @@ func (prodDB *ProductDB) GetProducts() ([]*entity.Product, error) {
 	var products []*entity.Product
 	for rows.Next() {
 		var product entity.Product
-		if err := rows.Scan(&product.ID, &product.Name); err != nil {
+		if err := rows.Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL); err != nil {
 			return nil, err
 		}
 		products = append(products, &product)
@@ -35,7 +35,9 @@ func (prodDB *ProductDB) GetProducts() ([]*entity.Product, error) {
 func (prodDB *ProductDB) GetProduct(id string) (*entity.Product, error) {
 	var product entity.Product
 
-	err := prodDB.db.QueryRow("SELECT id, name FROM products WHERE id = ?", id).Scan(&product.ID, &product.Name)
+	err := prodDB.db.
+		QueryRow("SELECT id, name, description, price, category_id, image_url FROM products WHERE id = ?", id).
+		Scan(&product.ID, &product.Name, &product.Description, &product.Price, &product.CategoryID, &product.ImageURL)
 	if err != nil {
 		return nil, err
 	}
