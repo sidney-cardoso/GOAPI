fb2eb9c271a3d48354ec975e33844163d9c25376
sidney-cardoso <sidneyrsc342@gmailcom>
2024-01-30 00:21:15 -0300

    Conexão com DB e roteamento

---
+++ cmd/catalog/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sidney-cardoso/goapi/internal/database"
+	"github.com/sidney-cardoso/goapi/internal/service"
+	"github.com/sidney-cardoso/goapi/internal/webserver"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/imersao17?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategoryDB(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	productDB := database.NewProductDB(db)
+	productService := service.NewProductService(*productDB)
+
+	webCategoryHandler := webserver.NewWebCategoryHandler(categoryService)
+	webProductHamdler := webserver.NewWebProductHandler(productService)
+
+	c := chi.NewRouter()
+	c.Use(middleware.Logger)
+	c.Use(middleware.Recoverer)
+	c.Get("/category/{id}", webCategoryHandler.GetCategory)
+	c.Get("/category", webCategoryHandler.GetCategories)
+	c.Post("/category", webCategoryHandler.CreateCategory)
+
+	c.Get("/product/{id}", webProductHamdler.GetProduct)
+	c.Get("/product", webProductHamdler.GetProducts)
+	c.Get("/product/category/{categoryID}", webProductHamdler.GetProductByCategory)
+	c.Post("/product", webProductHamdler.Createproduct)
+
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", c)
+}
